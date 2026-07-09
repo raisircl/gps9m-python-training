15e26f70e33821979f17f4d5e78581d82ce325d1
raisircl <rai.verma@live.com>
2026-07-09 09:57:48 +0530

    oops completed and magic methods also

--- Oops/Rect.py
+++ Oops/Rect.py
@@ -3,16 +3,39 @@ class Rect:
 
     count=0 # class attribute
     def __init__(self,len=1,bre=1): # constructor
-        self.l=len  # l and b is here instance attribute
-        self.b=bre
+        self.__l=len  # l and b is here instance attribute
+        self.__b=bre
         Rect.count+=1 # incrementing count for each object creation
         self.id=Rect.count # assigning unique id to each object
         self._extra="1" 
+    @property
+    def Length(self):
+        return self.__l
+    @Length.setter
+    def Length(self,value):
+        self.__l=value
+    @property
+    def Breadth(self):
+        return self.__b
+    @Breadth.setter
+    def Breadth(self,value):
+        self.__b=value
+
     def display(self):  # method
-        print(f"Dimension of Rect{self.id}: {self.l}x{self.b}")
+        print(f"Dimension of Rect{self.id}: {self.__l}x{self.__b}")
         
     def area(self): 
-        return self.l*self.b
+        return self.__l*self.__b
     def parimeter(self):
-        return 2*(self.l+self.b)
+        return 2*(self.__l+self.__b)
     
+    @classmethod
+    def get_count(cls):
+        return cls.count
+    def __str__(self):
+        return f"Rect{self.id} dimension: {self.__l}x{self.__b}"
+    def __add__(self,r2):
+        temp=Rect()
+        temp.Length=self.Length+r2.Length
+        temp.Breadth=self.Breadth+r2.Breadth
+        return temp
--- Oops/RectProgram.py
+++ Oops/RectProgram.py
@@ -1,20 +1,13 @@
 from Rect import Rect
 r1=Rect() # object 1
-r2=Rect() # object 2
+r2=Rect(4,2) # object 2
+r3=Rect() # object 3
 
-r3=Rect(3,4) # object 3
+r1.Length=5
+r1.Breadth=7
 
-r1.l=5
-r1.b=7
+r3=r1+r2 # adding two objects using operator overloading
 
-r1.display()
-r3.display()
-
-print(r1.greet)
-
-r1.greet="Hello I am using greet for me "
-print(r1.greet)
-print(r3.greet)
-
-print(f"Total Rect objects created: {Rect.count}")
-
+print(r1)
+print(r2)
+print(r3)
